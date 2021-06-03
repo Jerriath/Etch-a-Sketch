33a261c7818988c8036704cd0f8ff73e515becec
Jerry Zhang <jerryzhang101@yahoo.com>
2021-06-03 15:24:34 -0400

    Added rest of functions for making grid

--- scripts.js
+++ scripts.js
@@ -20,3 +20,29 @@ function promptLength() {
     return parseInt(prompt("How many squares would you like on each side?"));
 }
 
+//-----Function for coloring in the element that called-----
+function colorIn(element) {
+    element.style.background = "#000";
+}
+
+//-----Makes a grid with specified length-----
+function createGrid(length) {
+    for (let i = 0; i < length; i++)
+    {
+        for (let j = 0; j < length; j++)
+        {
+            let newElement = document.createElement("div"); 
+            newElement.classList.add("gridElement");
+            let side = 800/length; //dynamically resizes boxes depending on input
+            newElement.style.width = side + "px";
+            newElement.style.height = side + "px";
+            newElement.addEventListener("mouseover", function (e) {
+                colorIn(e.target); //colors in the boxes on mouseover event
+            });
+            grid.append(newElement);
+        }
+    }
+}
+
+//-----Calls the function to create grid-----
+createGrid(gridLength);
